4105850e2f1e126017e247f5811de5cde9d31e5e
Sandra Valadez <sandra.valadez.dl@gmail.com>
2018-11-05 22:24:38 -0600

    first commit

---
+++ js/loggin.js
@@ -0,0 +1,28 @@
+function modal(){
+  location.href = "login.html";
+}
+function carrito(){
+  location.href = "carrito.html";
+}
+function hist(){
+  location.href = "Historia.html";
+}
+function prod(){
+  console.log(",,");
+  location.href = "Productos.html";
+}
+window.onload = function() {
+
+
+var inicio=document.getElementById("loggin");
+inicio.addEventListener("click", modal, false);
+
+var histo=document.getElementById("hist");
+histo.addEventListener("click", hist, false);
+
+var produ=document.getElementById("prod");
+produ.addEventListener("click", prod, false);
+
+var compras=document.getElementById("compras");
+compras.addEventListener("click", carrito, false);
+}
