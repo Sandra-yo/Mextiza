ab4aaac1ca6332206375afe2c0ed21683832ed7a
Sandra <sandra.valadez.dl@gmail.com>
2018-12-01 15:29:09 -0600

    cambios

--- js/usuarioLogueado.js
+++ js/usuarioLogueado.js
@@ -2,22 +2,20 @@ function usuario() {
 window.onload = function() {  
 console.log("::::");
 
-if(localStorage.token!="null"){
+if(localStorage.token!="null" && localStorage.token!=undefined){
     console.log(":::");
     
-   console.log(localStorage.token=="null");
     $("#loggin a").html("Cerrar sesion");
     $("#loggin").addClass("SI");
     $("#compras").css("display","block");
 
     $("#compras").on("click",function () {
-        console.log(".s");
         location.href = "carrito.html";
         
     });
     
 $(".SI").on("click",function () {
-    console.log(";;;");
+    
     cerrarSesion();
     localStorage.email=null;
     localStorage.contraseña=null;
@@ -31,12 +29,29 @@ $(".SI").on("click",function () {
   });
 
     }else{
-        console.log(";E");
         $("#loggin a").html("Iniciar sesion");
         $("#loggin").removeClass("SI");
         $("#compras").css("display","none");
 $("#ordenar").on("click",function() {
     console.log(":3");
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "http://localhost:3000/api/Pedidos",
+        "method": "POST",
+        "headers": {
+          "authorization": "AdobJBPkVGwPn6CvOkUiM7iT7AA9hK2sFHknvPZskXPD6DpLkjG9tqi1xImQnoUG",
+          "content-type": "application/json",
+          "cache-control": "no-cache",
+          "postman-token": "a620d99b-c9ea-6638-a112-e9e4a4f454b1"
+        },
+        "processData": false,
+        "data": "{\n  \"estatus\": \"pendiente\",\n  \"fechaPedido\": \"20-11-18\",\n  \"fechaPagado\": \"20-11-18\",\n  \"sucursalId\": \"5bf3780615338534589f9528\",\n  \"usuarioId\": \"be3a52e432fba13bd6547c2\"\n}"
+      }
+      
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+      });
     
 });
  
